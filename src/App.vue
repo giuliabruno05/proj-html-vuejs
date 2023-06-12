941329f3e71f77b1295196d5181f319a4c30ca12
giuliabruno05 <giuliabruno48@gmail.com>
2023-06-12 11:10:15 +0200

    creazione componenti ed importazione in app vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <script>
+import PageHeader from './components/PageHeader.vue';
+import PageMain from './components/PageMain.vue';
+import PageFooter from './components/PageFooter.vue';
+export default {
+  components: {
+    PageHeader,
+    PageMain,
+    PageFooter
 
+  }
+}
 </script>
 
 <template></template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@use './styles/variables.scss' as *;
+@use './styles/general.scss' as *;
+</style>
